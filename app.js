484579e31a5666b1ba7c25e9d2d68e6ba4f09dbb
mariankomen <52856608+mariankomen@users.noreply.github.com>
2019-10-04 01:23:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-let fs = require('fs');
+const fs = require('fs');
 
-let std = require('./module1');
-let photo = require('./module2');
+const std = require('./module1');
+const photo = require('./module2');
 
-let student1 = std.student1;
-let student2 = std.student2;
-let student3 = std.student3;
-let student4 = std.student4;
-let student5 = std.student5;
-let student6 = std.student6;
+const student1 = std.student1;
+const student2 = std.student2;
+const student3 = std.student3;
+const student4 = std.student4;
+const student5 = std.student5;
+const student6 = std.student6;
 //Поміщаємо студентів в масив
-let arr = [student1,student2,student3];
-let arr2 = [student4,student5,student6];
+const arr = [student1,student2,student3];
+const arr2 = [student4,student5,student6];
 //додаємо до 28 групи
 fs.mkdir('js18' , () => {
     fs.writeFileSync('./js18/studFile', arr, () => {
@@ -25,16 +25,16 @@ fs.mkdir('js20' , () => {
     });
 });
 
-let myRead = fs.createReadStream(__dirname + '/js18/studFile', 'utf8');
-let myWrite = fs.createWriteStream(__dirname + '/js20/studFile2');
+const myRead = fs.createReadStream(__dirname + '/js18/studFile', 'utf8');
+const myWrite = fs.createWriteStream(__dirname + '/js20/studFile2');
 
 myRead.pipe(myWrite);
 
-let myRead1 = fs.createReadStream(__dirname + '/js20/studFile2', 'utf8');
-let myWrite1 = fs.createWriteStream(__dirname + '/js18/studFile');
+const myRead1 = fs.createReadStream(__dirname + '/js20/studFile2', 'utf8');
+const myWrite1 = fs.createWriteStream(__dirname + '/js18/studFile');
 
 myRead1.pipe(myWrite1);
 
-let p1 = photo.photo1;
-let p2 = photo.photo2;
-let p3 = photo.photo3;
+const p1 = photo.photo1;
+const p2 = photo.photo2;
+const p3 = photo.photo3;
